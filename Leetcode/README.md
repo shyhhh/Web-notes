bd18c587b95fb60a73f6488b5a331e04c77d693c
何宏贺 <hhhtheshy@gmail.com>
2022-07-20 10:23:26 +0800

    7.20

--- Leetcode/README.md
+++
@@ -1,7 +0,0 @@
-# Leetcode
-
-1. 可视化界面
-
-- [Algorithms](https://www.cs.usfca.edu/~galles/visualization/Algorithms.html)
-
-- [visualgo](https://visualgo.net/en)
--- Leetcode/手写LeetCode/tree/100相同树.js
+++
@@ -1,9 +0,0 @@
-var isSameTree = function(p, q) {
-  if(!q && !p) return true
-
-  if(q && p && q.val === p.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right)){
-    return true
-  }
-
-  return false
-};
